3ecb804b0e71c0505f7e59604879cd43f2690947
snomos <sjurnm@mac.com>
2016-05-04 12:10:13 +0300

    Several changes to both the third party build script and the project settings to accomodate additional requirements in new versions of hfst-ospell, plus the fact that it has moved to github. Some of the changes are kind of hacky, but seems to work for now. The ICU reliance needs to be verified, and should be made optional - it increased the size of the MacVoikko binary several times. But at least we seem to have a working version again, from which we can trim and change things further. Needs to be tested on another mac, though.

--- thirdparty/build.sh
+++ thirdparty/build.sh
@@ -26,6 +26,8 @@ if [[ ! -d libarchive-build ]]; then
 		  ../libarchive
 	make all install
 	cd "$ROOT"
+	install_name_tool -id @rpath/libarchive.dylib build/lib/libarchive.14.dylib
+	install_name_tool -id @rpath/libarchive.dylib build/lib/libarchive.dylib
 fi
 
 echo "\n** Building TinyXML2 **\n"
@@ -44,15 +46,28 @@ fi
 
 echo "\n** Building hfst-ospell **\n"
 
-if [[ ! -f hfst-ospell/hfst-ospell ]]; then
-	svn checkout svn://svn.code.sf.net/p/hfst/code/trunk/hfst-ospell
+if [[ ! -f build/lib/libhfstospell.dylib ]]; then
 	cd hfst-ospell
 	[[ -f configure ]] || ./autogen.sh
-	./configure --prefix="$ROOT/build" --enable-zhfst --with-tinyxml2 --without-libxmlpp
+	./configure --prefix="$ROOT/build" --enable-zhfst \
+				--with-tinyxml2=$ROOT/tinyxml2-build/ \
+				--without-libxmlpp \
+				CPPFLAGS=" -I$ROOT/build/include -L$ROOT/build/lib" \
+				LDFLAGS=" -I$ROOT/build/include -L$ROOT/build/lib" \
+				CFLAGS=" -I$ROOT/build/include -L$ROOT/build/lib" \
+				CXXFLAGS=" -I$ROOT/build/include -L$ROOT/build/lib" \
+				PKG_CONFIG_PATH=$ROOT/build/lib/pkgconfig
 	make all install
 	cd "$ROOT"
-	install_name_tool -id @rpath/libhfstospell.dylib build/lib/libhfstospell.4.dylib
+	install_name_tool -id @rpath/libhfstospell.dylib build/lib/libhfstospell.6.dylib
 	install_name_tool -id @rpath/libhfstospell.dylib build/lib/libhfstospell.dylib
+	# Hack to work around configure picking libraries from macports instead of local ones:
+	for i in $(otool -L build/lib/libhfstospell.dylib | fgrep '/opt/local/lib' \
+				| cut -f2 | cut -d' ' -f1); do
+		target=$(basename $i)
+		cp $i build/lib/$target
+		install_name_tool -change $i @rpath/$target build/lib/libhfstospell.dylib
+	done
 fi
 
 echo "\n** Building libvoikko **\n"
@@ -61,12 +76,21 @@ if [[ ! -f build/lib/libvoikko.dylib ]]; then
 	cd corevoikko/libvoikko
 	[[ -f configure ]] || ./autogen.sh
 	# Look in both /usr/local/lib/voikko and /Library/Spelling/voikko for dictionaries
-	./configure --enable-hfst --prefix="$ROOT/build" \
+	./configure --prefix="$ROOT/build" \
 	   --with-dictionary-path="/Library/Spelling/voikko:/usr/local/share/voikko:/usr/local/lib/voikko"
 	make all install
 	cd "$ROOT"
 	install_name_tool -id @rpath/libvoikko.dylib build/lib/libvoikko.1.dylib
 	install_name_tool -id @rpath/libvoikko.dylib build/lib/libvoikko.dylib
+	# Hack to work around configure picking libraries from macports instead of local ones:
+	for i in $(otool -L build/lib/libvoikko.dylib | fgrep '/opt/local/lib' \
+				| cut -f2 | cut -d' ' -f1); do
+		target=$(basename $i)
+		cp $i build/lib/$target
+		install_name_tool -change $i @rpath/$target build/lib/libvoikko.dylib
+	done
+	install_name_tool -change libarchive.14.dylib @rpath/libarchive.14.dylib build/lib/libvoikko.dylib
+	install_name_tool -change @rpath/libtinyxml2.3.dylib @rpath/libtinyxml2.3.0.0.dylib build/lib/libvoikko.dylib
 fi
 
 echo "\n** Building suomimalaga **\n"
